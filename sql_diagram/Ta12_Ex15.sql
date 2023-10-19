c15e83452a42a0a42dd22e42a587746fc7ec2cd1
Manel Castellví Cerezuela <manelcastellvicerezuela@gmail.com>
2023-10-19 18:27:44 +0200

    Updates y deletes 10 hechos, ex15

--- sql_diagram/Ta12_Ex15.sql
+++ sql_diagram/Ta12_Ex15.sql
@@ -85,21 +85,84 @@ INSERT INTO consisteMenuPlatos (id_menu_diario, id_plato_diario) VALUES
 UPDATE Platos SET nombre = 'Plato Actualizado 1' WHERE id = 1;
 UPDATE Platos SET descripcion = 'Nueva descripción 2' WHERE id = 2;
 UPDATE Platos SET tipo = 'Aperitivo' WHERE id = 3;
+UPDATE Platos SET nombre = 'Nuevo Nombre 4' WHERE id = 4;
+UPDATE Platos SET descripcion = 'Nueva descripción 5' WHERE id = 5;
+UPDATE Platos SET tipo = 'Aperitivo' WHERE id = 6;
+UPDATE Platos SET nombre = 'Nuevo Nombre 7' WHERE id = 7;
+UPDATE Platos SET descripcion = 'Nueva descripción 8' WHERE id = 8;
+UPDATE Platos SET tipo = 'Aperitivo' WHERE id = 9;
+UPDATE Platos SET nombre = 'Nuevo Nombre 10' WHERE id = 10;
 
 UPDATE MenuDiario SET fecha_menu = '2023-10-24' WHERE id = 1;
 UPDATE MenuDiario SET temperatura_del_dia = 26 WHERE id = 2;
 UPDATE MenuDiario SET num_personas_tomado = 48 WHERE id = 3;
+UPDATE MenuDiario SET fecha_menu = '2023-10-25' WHERE id = 4;
+UPDATE MenuDiario SET temperatura_del_dia = 27 WHERE id = 5;
+UPDATE MenuDiario SET num_personas_tomado = 42 WHERE id = 6;
+UPDATE MenuDiario SET fecha_menu = '2023-10-26' WHERE id = 7;
+UPDATE MenuDiario SET temperatura_del_dia = 28 WHERE id = 8;
+UPDATE MenuDiario SET num_personas_tomado = 50 WHERE id = 9;
+UPDATE MenuDiario SET fecha_menu = '2023-10-27' WHERE id = 10;
 
 UPDATE PlatosDiarios SET cantidad_platos_servidos = 40 WHERE id = 1;
 UPDATE PlatosDiarios SET id_plato = 3 WHERE id = 2;
+UPDATE PlatosDiarios SET cantidad_platos_servidos = 35 WHERE id = 3;
+UPDATE PlatosDiarios SET id_plato = 2 WHERE id = 4;
+UPDATE PlatosDiarios SET cantidad_platos_servidos = 30 WHERE id = 5;
+UPDATE PlatosDiarios SET id_plato = 4 WHERE id = 6;
+UPDATE PlatosDiarios SET cantidad_platos_servidos = 25 WHERE id = 7;
+UPDATE PlatosDiarios SET id_plato = 1 WHERE id = 8;
+UPDATE PlatosDiarios SET cantidad_platos_servidos = 18 WHERE id = 9;
+UPDATE PlatosDiarios SET id_plato = 5 WHERE id = 10;
+
 
 -- -----------------------------------DELETES------------------------------------------------
+DELETE FROM Platos WHERE id = 6;
+DELETE FROM Platos WHERE id = 7;
+DELETE FROM Platos WHERE id = 8;
+DELETE FROM Platos WHERE id = 9;
+DELETE FROM Platos WHERE id = 10;
+DELETE FROM Platos WHERE id = 11;
+DELETE FROM Platos WHERE id = 12;
+DELETE FROM Platos WHERE id = 13;
+DELETE FROM Platos WHERE id = 14;
+DELETE FROM Platos WHERE id = 15;
 
-DELETE FROM Platos WHERE id = 4;
-DELETE FROM Platos WHERE id = 5;
+DELETE FROM MenuDiario WHERE id = 11;
+DELETE FROM MenuDiario WHERE id = 12;
+DELETE FROM MenuDiario WHERE id = 13;
+DELETE FROM MenuDiario WHERE id = 14;
+DELETE FROM MenuDiario WHERE id = 15;
+DELETE FROM MenuDiario WHERE id = 16;
+DELETE FROM MenuDiario WHERE id = 17;
+DELETE FROM MenuDiario WHERE id = 18;
+DELETE FROM MenuDiario WHERE id = 19;
+DELETE FROM MenuDiario WHERE id = 20;
 
-DELETE FROM MenuDiario WHERE id = 1;
-DELETE FROM MenuDiario WHERE id = 2;
+DELETE FROM PlatosDiarios WHERE id = 11;
+DELETE FROM PlatosDiarios WHERE id = 12;
+DELETE FROM PlatosDiarios WHERE id = 13;
+DELETE FROM PlatosDiarios WHERE id = 14;
+DELETE FROM PlatosDiarios WHERE id = 15;
+DELETE FROM PlatosDiarios WHERE id = 16;
+DELETE FROM PlatosDiarios WHERE id = 17;
+DELETE FROM PlatosDiarios WHERE id = 18;
+DELETE FROM PlatosDiarios WHERE id = 19;
+DELETE FROM PlatosDiarios WHERE id = 20;
 
-DELETE FROM PlatosDiarios WHERE id = 3;
-DELETE FROM PlatosDiarios WHERE id = 4;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 11;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 12;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 13;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 14;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 15;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 16;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 17;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 18;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 19;
+DELETE FROM consisteMenuPlatos WHERE id_menu_diario = 20;
+
+DELETE FROM Platos WHERE id = 16;
+DELETE FROM Platos WHERE id = 17;
+DELETE FROM Platos WHERE id = 18;
+DELETE FROM Platos WHERE id = 19;
+DELETE FROM Platos WHERE id = 20;
